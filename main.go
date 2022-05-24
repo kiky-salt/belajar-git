67181744e9148b9b836819c6093126a50fa8c9b0
renaldid <purwantorenaldi@gmail.com>
2022-05-24 21:08:08 +0700

    feat:update password

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hello world")
 	login()
+	updatepassword()
 }
 
 func login() {
 	fmt.Println("Login")
+}
+
+func updatepassword() {
+	fmt.Println("Update Password")
 }
